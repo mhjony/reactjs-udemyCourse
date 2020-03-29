650f3a118a7719e8616e589d7dd378cf13ffd4ec
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2020-03-29 20:25:36 +0300

    understanding of props and state

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ class App extends Component {
       persons: [
         { name: 'Mahmudul', age: '82' },
         { name: 'Mehedi', age: '12' }
-      ]
+      ],
+      otherState: 'some other value'
     }
+
+  clickNameHendler = () => {
+    this.setState({
+      persons: [
+        { name: 'Mahmudul Hasan', age: '28' },
+        { name: 'Mehedi Hasan', age: '18' }
+      ]
+    })
+  }
   render() {
     return (
       <div className="App" >
         <h1>Hi, I am a react app</h1>
-        <button>Switch</button>
+        <button onClick={this.clickNameHendler}>Switch</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My brother</Person>
+        <Person name={this.state.otherState} />
       </div>
     );
     //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'am a react app'));
